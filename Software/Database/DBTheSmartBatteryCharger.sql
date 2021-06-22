f2fd957a111a9e4ed47dbb12427b9143341db007
Sherif .M Samy <eng.SherifMostafa25@gmail.com>
2021-06-22 10:13:30 +0200

    ch 1 without formatting

--- Software/Database/DBTheSmartBatteryCharger.sql
+++ Software/Database/DBTheSmartBatteryCharger.sql
@@ -1,24 +1,28 @@
-create database DBTheSmartBatteryCharger
+--create main database
+CREATE DATABASE DBTheSmartBatteryCharger
 
-use [DBTheSmartBatteryCharger]
-go
+--go to main database
+USE [DBTheSmartBatteryCharger]
+GO
 
-create table tblLogFile
+--create main table
+CREATE TABLE tblLogFile
 (
-	colIndex int identity(1,1) primary key,
-	colDate date not null,
-	colTime time not null,
-	colBatteryPercent int not null,
-	colChargerStatus char(7) not null constraint colChargerStatus_Chk check (colChargerStatus in ('Online','Offline')),
-	colCpuPerformance int not null,
-	colRamPerformance int not null,
-	colHardDiskPerformance int not null
+	colIndex INT IDENTITY(1,1) PRIMARY KEY,
+	colDate DATE NOT NULL,
+	colTime TIME NOT NULL,
+	colBatteryPercent INT NOT NULL,
+	colChargerStatus CHAR(7) NOT NULL CONSTRAINT colChargerStatus_Chk CHECK (colChargerStatus IN ('Online','Offline')),
+	colCpuPerformance INT NOT NULL,
+	colRamPerformance INT NOT NULL,
+	colHardDiskPerformance INT NOT NULL
 )
 
-insert into tblLogFile values
+--insert statement
+INSERT INTO tblLogFile VALUES
 (
-	(format (GETDATE(), 'dd MMMM yyyy')), 
-	(format (GETDATE(), 'hh:mm:ss tt')), 
+	(FORMAT (GETDATE(), 'dd MMMM yyyy')), 
+	(FORMAT (GETDATE(), 'hh:mm:ss tt')), 
 	70, 
 	'Offline',
 	50,
@@ -26,14 +30,75 @@ insert into tblLogFile values
 	2
 )
 
-delete from [tblLogFile] where colIndex = 5
+--delete statement
+DELETE FROM [tblLogFile] WHERE colIndex = 154
 
-Select colIndex as [Index], 
-format ([colDate], 'dd MMMM yyyy') as [Date], 
-(SELECT format (cast ([colTime] as datetime), 'hh:mm:ss tt')) as [Time], 
-[colBatteryPercent] as [Battery Status], 
-trim([colChargerStatus]) as [Charger Status],
-[colCpuPerformance] as [Cpu Utilization],
-[colRamPerformance] as [Ram Utilization],
-[colHardDiskPerformance] as [Hard Disk Utilization]
-from [tblLogFile]
+--select * from main table
+SELECT colIndex AS [Index],
+FORMAT ([colDate], 'dd MMMM yyyy') AS [Date],
+(SELECT FORMAT (CAST ([colTime] AS DATETIME), 'hh:mm:ss tt')) AS [Time],
+[colBatteryPercent] AS [Battery Status],
+TRIM([colChargerStatus]) AS [Charger Status],
+[colCpuPerformance] AS [Cpu Utilization],
+[colRamPerformance] AS [Ram Utilization],
+[colHardDiskPerformance] AS [Hard Disk Utilization]
+FROM [tblLogFile]
+
+
+SELECT
+		(SELECT MIN(CONCAT(colDate, 'T', colTime, '+02:00')) WHERE [colChargerStatus] = [colChargerStatus]) AS [First Date],
+		(SELECT MAX(CONCAT(colDate, 'T', colTime, '+02:00')) WHERE [colChargerStatus] = [colChargerStatus]) AS [Last Time],
+
+		(DATEDIFF
+		(
+			SECOND,
+			(SELECT MIN(CONCAT(colDate, 'T', colTime, '+02:00')) WHERE [colChargerStatus] = [colChargerStatus]),
+			(SELECT MAX(CONCAT(colDate, 'T', colTime, '+02:00')) WHERE [colChargerStatus] = [colChargerStatus]))
+		)
+		AS [Difference of Date and Time],
+
+		TRIM([colChargerStatus]) AS [Charger Status],
+
+		(SELECT COUNT([colChargerStatus])) AS [Count of Charge / Discharge],
+
+		AVG([colCpuPerformance]) AS [Average of Cpu Utilization],
+		AVG([colRamPerformance]) AS [Average of Ram Utilization],
+		AVG([colHardDiskPerformance]) AS [Average of Hard Disk Utilization]
+
+FROM tblLogFile
+GROUP By [colChargerStatus]
+ORDER BY [colChargerStatus] DESC
+
+--create view for report
+CREATE VIEW [vWReport]
+	AS
+	SELECT
+		ColChargerStatus,
+
+		PARSE(CONCAT(colDate,' ', colTime) AS DATETIME) AS [Current Date],
+		LAG(PARSE(CONCAT(colDate, ' ', colTime) AS DATETIME), 1, NULL) OVER(ORDER BY colDate, colTime) AS [Lag Date],
+	
+		colBatteryPercent,
+		LAG(colBatteryPercent, 1, NULL) OVER(ORDER BY colBatteryPercent) AS [colLeadBatteryPercent]
+	FROM tblLogFile
+
+----------------------------------------------------------------------------------------------------------------------------------
+
+SELECT
+	*,
+	(colBatteryPercent - colLeadBatteryPercent) AS [colBatteryPercentDiff],
+	
+	(
+		DATEDIFF
+			(
+				SECOND,
+				[Current Date],
+				[Lag Date]
+			)
+	)	AS [colDateTimeDiff]
+
+FROM vWReport
+ORDER BY [Lag Date]
+
+
+exec sp_help vWReport
